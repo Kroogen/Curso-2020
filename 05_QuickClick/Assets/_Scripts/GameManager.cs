c24a1c1c6d54cf77e4a6b18fd8ed972c05172b51
Kroogen <74164935+Kroogen@users.noreply.github.com>
2021-01-10 20:39:42 -0600

    GameManager
    Creación del game manager e implementación de Instancias para los Targets (elementos que vuelan)

---
+++ 05_QuickClick/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targetPrefabs;
+    public float spawnRate = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarger());
+    }
+
+    IEnumerator SpawnTarger()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            Instantiate(targetPrefabs[index]);
+        }
+    }
+
+}
--- 05_QuickClick/Assets/_Scripts/Target.cs
+++ 05_QuickClick/Assets/_Scripts/Target.cs
@@ -4,10 +4,36 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+
+    private Rigidbody _rigidbody;
+    public float minForce = 14.5f;
+    public float maxForce = 17;
+    public float torqueRange = 10;
+    public float positionLimitX = 4;
+    public float positionLimitY = -5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(),ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(),RandomTorque(),RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-positionLimitX, positionLimitX),positionLimitY);//z=0
+    }
+
+    private float RandomTorque()
+    {
+        return Random.Range(-torqueRange, torqueRange);
+    }
+
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
     }
 
     // Update is called once per frame
