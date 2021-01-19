fa3795766ec79474d6113d0349bdb80a9e2e1ca8
Kroogen <74164935+Kroogen@users.noreply.github.com>
2021-01-18 22:58:37 -0600

    John Lemon avance
    Se añadió post procesado a la imagen y se configuró el cine machine, tambien se añadieron efectos y la zona por donde los NPC estarán

---
+++ JohnLemon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector3 movement;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+    public float turnSpeed = 20;
+    private Vector3 desireForward;
+    private Quaternion rotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement.Set(horizontal,0,vertical);
+        movement.Normalize();
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        _animator.SetBool("IsWalking",isWalking);
+        desireForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.fixedDeltaTime, 0);
+        rotation = Quaternion.LookRotation(desireForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
